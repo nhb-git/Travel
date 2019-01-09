b9cdf4d61d1ba95602e29680a4e5fa5ecd0b3d38
nhb <niuhaibao@gmail.com>
2019-01-10 00:48:14 +0800

    header css style

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="header-left">fan</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">cheng</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      height: .62rem
+      line-height: .62rem
+      margin-top: .12rem
+      margin-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+      text-align: left
+    .header-right
+      width: 1.24rem
+      float: ritht
+      text-align: center
+</style>
